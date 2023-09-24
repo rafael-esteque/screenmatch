9aa17e230d20d3589271bbee5abecbf1898f1c70
Rafael <rafaelgesteque@gmail.com>
2023-09-24 15:36:01 -0300

    aula 01 completa

---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Filme favorito = new Filme();
+
+        favorito.nome = "The Matrix";
+        favorito.anoDeLancamento = 1999;
+        favorito.duracaoEmMinutos = 135;
+        favorito.incluidoNoPlano = true;
+
+        favorito.exibeFichaTecnica();
+        favorito.avalia(9);
+        favorito.avalia(8);
+        favorito.avalia(9);
+
+        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+    }
+
+}
